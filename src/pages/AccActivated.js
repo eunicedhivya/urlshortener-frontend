3555804e82a1a142a98942503b6e687bdbdc472d
Eunice Dhivya <divi.seed@gmail.com>
2022-06-29 23:24:45 +0530

    create an account activated page

---
+++ src/pages/AccActivated.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function AccActivated() {
+  return (
+    <div className="mt-5">
+      <h1 className="text-primary">Account has been activated.</h1>
+      <p>
+        Please{" "}
+        <a className="text-primary" href="/login">
+          click here
+        </a>{" "}
+        to login
+      </p>
+    </div>
+  );
+}
+
+export default AccActivated;
